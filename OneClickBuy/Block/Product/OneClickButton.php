eac121f67c0b38dc57a9e120315736172ef1f759
igorpilat <pilatigor039@gmail.com>
2023-08-28 19:39:05 +0300

    add ajax call

--- OneClickBuy/Block/Product/OneClickButton.php
+++ OneClickBuy/Block/Product/OneClickButton.php
@@ -3,8 +3,36 @@
 namespace Amida\OneClickBuy\Block\Product;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Block\Product\View as ProductView;
 
 class OneClickButton extends Template
 {
+    /**
+     * @var ProductView
+     */
+    protected ProductView $product;
 
+    public function __construct(
+        Template\Context $context,
+        ProductView $product,
+        array $data = []
+    )
+    {
+        $this->product = $product;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Get current product SKU
+     *
+     * @return false|string
+     */
+    public function getProductSku()
+    {
+        $product = $this->product->getProduct();
+        if ($product) {
+            return $product->getSku();
+        }
+        return false;
+    }
 }
